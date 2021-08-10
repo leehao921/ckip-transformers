410237be2bc5de4228b8763f07513045f760c3a3
lee <leehao921@gmail.com>
2021-08-10 10:15:23 +0800

    Ver 1.0.0 have done prettily
    patant tech and result analysis havnt done yet
    the rest are all done :)

--- example/fileConcat.py
+++ example/fileConcat.py
@@ -4,7 +4,7 @@ from pandas import ExcelWriter
 import pandas as pd
 
 tmp_data=[]
-for fileName in ["中信金控", "兆豐金控", "台新金控", "台灣金融控股", "國泰金控", "第一金控", "玉山金控", "土地銀行", "富邦金控", "開發金", "新光金控"]:
+for fileName in ["中信金控", "兆豐金控", "台新金控", "台灣金融控股", "國泰金控", "第一金控", "玉山金控", "富邦金控", "新光金控"]:
     fileloc = "./_target/"+fileName+"reslut.xls"
     file = pd.read_excel(fileloc)
     tmp_data.append(file)
